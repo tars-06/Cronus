d51cd1250b2d2b59adb131f70809da22941f3728
tars-06 <srfaadi@gmail.com>
2025-08-15 18:17:11 +0530

    working 3

--- backend/index.js
+++ backend/index.js
@@ -89,7 +89,7 @@ const __dirname = path.resolve()
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-app.get('/', (req,res)=>{
+app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
 })
 

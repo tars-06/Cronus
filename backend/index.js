15cda71d2f3fa8276fa5dd2d4896969aa20ccada
tars-06 <srfaadi@gmail.com>
2025-08-15 18:29:12 +0530

    downgraded express to make it work

--- backend/index.js
+++ backend/index.js
@@ -86,11 +86,11 @@ io.on("connection",(socket)=>{
 const PORT = process.env.PORT || 5000
 
 const __dirname = path.resolve()
-
-app.use(express.static(path.join(__dirname, "../frontend/dist")))
+const frontendPath = path.join(__dirname, "./frontend/dist");
+app.use(express.static(frontendPath))
 
 app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+    res.sendFile(path.join(frontendPath, "index.html"))
 })
 
 
